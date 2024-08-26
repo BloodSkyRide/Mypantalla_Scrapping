24e91a5744e3efd12caf4fbfbcb59c5867105851
BloodSkyRide <90225273+BloodSkyRide@users.noreply.github.com>
2024-08-25 21:09:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ async function openWebPage(){ // declaramos una funcion asincrona
     const page = await browser.newPage(''); // se declara que va abrir una nueva pagina del navegador
     
     await page.goto('https://accounts.spotify.com/es/login?continue=https%3A%2F%2Fopen.spotify.com%2Fintl-es'); // declaramos la ruta a la cual va ingresar
-    await page.type('#login-username','santiago.gallego1@utp.edu.co'); // buscamos el id del input del username, posteriormente proporcionamos el username
-    await page.type('#login-password','Dualshock2274');// buscamos el id del input del password, posteriormente proporcionamos el password
+    await page.type('#login-username',''); // buscamos el id del input del username, posteriormente proporcionamos el username
+    await page.type('#login-password','');// buscamos el id del input del password, posteriormente proporcionamos el password
     await page.click('#login-button');// por ultimo click al boton de iniciar sesion
     await page.waitForNavigation({ waitUntil: 'networkidle2' });// como estamos cambiando de pagina esperamos que se termine de cargar toda la pagina tras el login
     await page.waitForSelector('div[role="presentation"][style*="transform: translateY(0px)"]'); // por seguridad esperamos a que ese node con esas caracteriusticas este bien formado
